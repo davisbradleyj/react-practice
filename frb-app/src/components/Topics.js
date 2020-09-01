26efbc80432e3069148b910a3f45fac95e14be92
guachochi <davis.bradleyj@gmail.com>
2020-09-01 14:37:08 -0700

    update link and path to allow for nested components

--- frb-app/src/components/Topics.js
+++ frb-app/src/components/Topics.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Link, Route } from 'react-router-dom'
 
-function Topic({ match }) {
-  return <h3>{match.params.topicId}</h3>
+function Topic(props) {
+  return <h3>{props.match.params.topicId}</h3>
 }
 
-function Topics() {
+function Topics({match}) {
+  console.log('match.url', match.url)
   return (
     <div>
       <h3>Topics</h3>
       <ul>
-        <li><Link to='/topics/rendering'>Rendering with React</Link></li>
-        <li><Link to='/topics/components'>Components</Link></li>
-        <li><Link to='/topics/props-v-state'>Props vs State</Link></li>
+        <li><Link to={`${match.url}/rendering`}>Rendering with React</Link></li>
+        <li><Link to={`${match.url}/components`}>Components</Link></li>
+        <li><Link to={`${match.url}/props-v-state`}>Props vs State</Link></li>
       </ul>
 
       <hr />
 
-      <Route path='/topics/:topicId' component={Topic} />
+      <Route path={`${match.path}/:topicId`} component={Topic} />
 
     </div>
   )
