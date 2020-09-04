be1be3adc22a14d209e2075aefbc2cb0cbb72110
guachochi <davis.bradleyj@gmail.com>
2020-09-04 08:58:49 -0700

    update App.js to include 'extends Component' logic, review 'bind' and passing methods between components

--- udemy-course/src/App.js
+++ udemy-course/src/App.js
@@ -1,40 +1,97 @@
 import React, { Component } from 'react';
+// import React, { setState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
+// const app = props => {
+
+//   let [personState, setPersonState] = useState(
+//     {
+//       persons: [{
+//         name: 'brad',
+//         age: '37'
+//       }, {
+//         name: 'max',
+//         age: '28'
+//       }]
+//     })
+//   const switchNameHandler = () => {
+//     console.log('was clicked')
+//     setPersonState({
+//       persons: [{
+//         nanme: 'cristina',
+//         age: '28'
+//       }, {
+//         nanme: 'dan',
+//         age: '28'
+//       }]
+//     })
+//   }
+
+//   return (
+//     <div className='App'>
+//       <h1>Hi, I'm a React App</h1>
+//       <p>..and I'm working!</p>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person
+//         name={personState.person[0].name}
+//         age={personState.person[0].age} />
+//       <Person
+//         name={personState.person[1].name}
+//         age={personState.person[1].age} />
+//     </div>
+//   )
+// }
+
+// **************************************************************
+// OLD WAY of creating components:
+
+
 class App extends Component {
+  // this will show the same output but using the state property, if the state values are change the page will get re-render
+  state = {
+    persons: [{
+      name: "max",
+      age: "28",
+    }, {
+      name: "manu",
+      age: "30",
+    }],
+    otherAttribute: "something"
+  }
+  // when assinging a method it becomes a function, it will be a property that witholds a function
+  switchNameHandler = (newName) => {
+    console.log("was clicked!");
+    // DON'T DO THIS:  this.state.persons[0].name = "ana"; to change a value of the state
+    this.setState({ 
+      persons: [{ 
+        name: newName, 
+        age: "28", 
+      }, { 
+        name: "ana", 
+        age: "32", 
+      }] 
+    });
+  }
   render() {
     return (
-      <div className='App'>
-        <h1>Hi, I'm a React App</h1>
-        <p>This is a working app</p>
-        <Person />
-        <Age />
+      <div className="App">
+        <h1>Hi, I'm a React APP</h1>
+        <p>This is really working!</p>
+        {/* adding event handler */}
+        {/* when assinging the handler do not use () in the end, other wise it will execute the function immediately.  we just want to pass a reference to the function */}
+        <button onClick={this.switchNameHandler.bind(this, 'Maximillian')}>Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} 
+        click={this.switchNameHandler.bind(this, 'Max!')}/>
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age} />
       </div>
-    )
+    );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
--- udemy-course/src/Person/Person.js
+++ udemy-course/src/Person/Person.js
@@ -1,37 +1,10 @@
 import React from 'react';
 
-const person = props => {
-
-  let [personState, setPersonState] = useState(
-    {
-      persons: [{
-        name: 'brad',
-        age: '37'
-      }, {
-        name: 'max',
-        age: '28'
-      }]
-    })
-  const switchNameHandler = () => {
-    console.log('was clicked')
-    setPersonState({
-      persons: [{
-        nanme: 'cristina',
-        age: '28'
-      }, {
-        nanme: 'dan',
-        age: '28'
-      }]
-    })
-  }
-
+const person = ( props ) => {
   return (
-    <div className='App'>
-      <h1>Hi, I'm a React App</h1>
-      <p>..and I'm working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personState.person[0].name} age={personState.person[0].age} />
-      <Person name={personState.person[1].name} age={personState.person[1].age} />
+    <div>
+      <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+      <p>{props.children}</p>
     </div>
   )
 }
