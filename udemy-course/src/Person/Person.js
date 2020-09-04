f8abef69dce69d16d99a7fe371fc3c7bd1b869c2
guachochi <davis.bradleyj@gmail.com>
2020-09-04 08:20:05 -0700

    restructure folder structure, develop Person.js

--- udemy-course/src/Person/Person.js
+++ udemy-course/src/Person/Person.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
-const person = () => {
+const person = props => {
+
+  let [personState, setPersonState] = useState(
+    {
+      persons: [{
+        name: 'brad',
+        age: '37'
+      }, {
+        name: 'max',
+        age: '28'
+      }]
+    })
+  const switchNameHandler = () => {
+    console.log('was clicked')
+    setPersonState({
+      persons: [{
+        nanme: 'cristina',
+        age: '28'
+      }, {
+        nanme: 'dan',
+        age: '28'
+      }]
+    })
+  }
+
   return (
-    <div>
-      <p>I'm a Person!</p>
+    <div className='App'>
+      <h1>Hi, I'm a React App</h1>
+      <p>..and I'm working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personState.person[0].name} age={personState.person[0].age} />
+      <Person name={personState.person[1].name} age={personState.person[1].age} />
     </div>
   )
 }
+
+// class Person extends Component {
+//   state = ({
+//     persons: [{
+//       name: 'brad',
+//       age: '37'
+//     }, {
+//       name: 'max',
+//       age: '28'
+//     }]
+//   })
+// }
 
 export default person;
