c2d48aab664495ac7fa22a53eff50984b0a01533
guachochi <davis.bradleyj@gmail.com>
2020-09-04 08:20:56 -0700

    create and track new directories in linkedin1 project

--- linkedin1/src/index.js
+++ linkedin1/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import 'jquery/dist/jquery.js';
-import 'popper.js/dist/umd/popper';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'jquery/dist/jquery/jquery.js'
+import 'popper.js/dist/umd/popper.js'
+import 'bootstrap/dist/js/bootstrap.js'
 
 ReactDOM.render(
   <React.StrictMode>
