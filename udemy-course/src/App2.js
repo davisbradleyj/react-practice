ace0af7758da52f4235c1aed5ad4a61147494e4b
guachochi <davis.bradleyj@gmail.com>
2020-09-13 21:39:32 -0700

    Section 4 assignment: add logic to delete a Char card, while corresponding text reflects the removed card

--- udemy-course/src/App2.js
+++ udemy-course/src/App2.js
@@ -9,23 +9,33 @@ class App2 extends Component {
   }
 
   inputChangeHandler = (e) => {
-    this.setState({userInput: e.target.value})
+    this.setState({ userInput: e.target.value })
+  }
+
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index,1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
   }
 
   render() {
-    const charList = this.state.userInput.split('').map(ch, index => {
-      return <Char character={ch} key={index} />
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char
+        character={ch}
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />
     })
-    
+
     return (
-    
+
       <div className='App'>
         <input
           type='text'
           onChange={this.inputChangeHandler}
           value={this.state.userInput} />
         <p>{this.state.userInput}</p>
-        <Validation inputLength={this.state.userInput.length}/>
+        <Validation inputLength={this.state.userInput.length} />
         {charList}
       </div>
     )
