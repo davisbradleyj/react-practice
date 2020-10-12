06e92b0c6052ea0341cc23c197acec212b5ed334
guachochi <davis.bradleyj@gmail.com>
2020-10-11 21:51:15 -0700

    create handler and logic to add ingredients to burger; pass added ingredient logic to Controls component then on to the 'more' button in the Control component

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,13 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const ING_PRICES = {
+  salad: 0.49,
+  cheese: 0.49,
+  meat: 1.49,
+  bacon: 0.99,
+}
+
 class BurgerBuilder extends Component {
   state = {
     ingredient: {
@@ -11,14 +18,33 @@ class BurgerBuilder extends Component {
       bacon: 0,
       cheese: 0,
       meat: 0,
-    }
+    },
+    totalPrice: 3.99
   }
+
+  addIngHandler = (type) => {
+    let oldCount = this.state.ingredient[type];
+    let updatedCount = oldCount++;
+    let updatedIng = {
+      ...this.state.ingredient
+    };
+    updatedIng[type] = updatedCount;
+    let priceAddition = ING_PRICES[type];
+    let oldPrice = this.state.totalPrice;
+    let newPrice = oldPrice + priceAddition;
+    this.setState({totalPrice: newPrice, ingredient: updatedIng})
+  }
+
+  removeIngHandler = (type) => {
+
+  } 
 
   render () {
     return (
       <Aux>
         <Burger ingredient={this.state.ingredient}/>
-        <BuildControls />
+        <BuildControls 
+          ingredientAdded={this.addIngHandler}/>
       </Aux>
     );
   };
