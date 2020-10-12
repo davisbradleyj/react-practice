7c73c2dee0fc65f25d49c9065a982f2720178352
guachochi <davis.bradleyj@gmail.com>
2020-10-11 21:17:45 -0700

    use reduce to flatten ingredient list and add a check to see if ingredients have been added to Burger

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,14 +4,22 @@ import classes from './Burger.module.css';
 import Ingredients from './Ingredients/Ingredients';
 
 const burger = (props) => {
-  const incomingIngredients = Object.keys(props.ingredient) // default JS object w/ keys method to extract keys from an object
+  let incomingIngredients = Object.keys(props.ingredient) // default JS object w/ keys method to extract keys from an object
     .map(ingKey => {
       return [...Array(props.ingredient[ingKey])] // arr w/ 2 elements [ , ]
-        .map((_,i) => {
+        .map((_, i) => {
           return <Ingredients key={ingKey + i} type={ingKey} />
-        }) ;
-    });
-   return (
+        });
+    })
+    .reduce((arr,el) => {
+      return arr.concat(el)
+    }, []);
+  if (incomingIngredients.length === 0) {
+    incomingIngredients = <p> Please add some ingredients! </p>
+  }
+  console.log(incomingIngredients);
+
+  return (
     <div className={classes.Burger}>
       <Ingredients type='bun-top' />
       {incomingIngredients}
