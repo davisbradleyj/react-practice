07e0facbada9d8a9ae4114f92ebc36ec9d9b6bd7
guachochi <davis.bradleyj@gmail.com>
2020-10-20 12:44:05 -0700

    add NavigationItem(s) to Navigation directory, and js files

---
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const navigItem = (props) => (
+  <li><a href='/'>A Link</a></li>
+);
+
+export default navigItem
---
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const navigItems = () => (
+ <ul>
+   <li><a href='/'>A Link</a></li>
+ </ul>
+);
+
+export default navigItems
