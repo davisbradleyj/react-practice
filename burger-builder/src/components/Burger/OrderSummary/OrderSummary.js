f4be043dbf6926889d7ed96f7a9df789d427b0d3
guachochi <davis.bradleyj@gmail.com>
2020-10-25 22:35:35 -0700

    add checks to see if a component should update; modify functional component to class component to test

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingSummary = Object.keys(props.ingredient)
-    .map(ingKey => {
-      return (<li key={ingKey}>
-        <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>: {props.ingredient[ingKey]}
-      </li>);
-    });
+class OrderSummary extends Component {
+  // could remain as a function component, not a class
+  componentDidUpdate() {
+    console.log('Order Summary Will Update')
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <ul>
-        {ingSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      {/* Opportunity to create a repeatable element with styling, such as Continue or Cancel buttons */}
-      <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-      <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-    </Aux>
+  render() {
+    const ingSummary = Object.keys(this.props.ingredient)
+      .map(ingKey => {
+        return (<li key={ingKey}>
+          <span style={{ textTransform: 'capitalize' }}>{ingKey}</span>: {this.props.ingredient[ingKey]}
+        </li>);
+      });
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <ul>
+          {ingSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        {/* Opportunity to create a repeatable element with styling, such as Continue or Cancel buttons */}
+        <Button btnType='Success' clicked={this.props.purchaseContinued}>Continue</Button>
+        <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+      </Aux>
+    );
+  };
+}
 
-  )
-};
-
-export default orderSummary;
+export default OrderSummary;
