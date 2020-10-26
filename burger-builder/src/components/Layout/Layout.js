be4c2e14f1a2dfeaa6ed6189f9d8603e28b95ef5
guachochi <davis.bradleyj@gmail.com>
2020-10-25 21:53:32 -0700

    modify layout to be class based component; add logic to close sidedrawer

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import Sidedrawer from '../Navigation/Sidedrawer/Sidedrawer';
 
-const layout = ( props ) => (
-  <Aux>
-    <Toolbar />
-    <Sidedrawer />
-  <main className={classes.Content}>
-    {props.children}
-  </main>
-  </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+  state = {
+    showSidedrawer: true
+  }
+
+  sidedrawerCloseHandler = () => {
+    this.setState({ showSidedrawer: false });
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <Sidedrawer
+          open={this.state.showSidedrawer}
+          closed={this.sidedrawerCloseHandler} />
+            <main className={classes.Content}>
+              {this.props.children}
+            </main>
+      </Aux>
+    )
+  }
+}
+
+export default Layout;
--- burger-builder/src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ burger-builder/src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -3,18 +3,30 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './Sidedrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux'
 
 const sidedrawer = (props) => {
-  // ...
+  let attachedClasses = [classes.Sidedrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.Sidedrawer, classes.Open];
+  }
+
+
   return (
-    <div className={classes.Sidedrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop
+        show={props.open}
+        clicked={props.closed} />
+      <div className={attachedClasses.join(' ')}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 }
 
