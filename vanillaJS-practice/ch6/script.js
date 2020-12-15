f7f2545f0aebc99d90d3222d46af22e8b42cd688
guachochi <davis.bradleyj@gmail.com>
2020-12-15 12:06:40 -0800

    Modify code to account for counterclockwise reset at 0 by letting the Pos counter continue to add an incrimental value going forward

--- vanillaJS-practice/ch6/script.js
+++ vanillaJS-practice/ch6/script.js
@@ -2,20 +2,21 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+// console.log("The Time is " + hr + ":" + min + ":" + sec)
+
+let hrPos = (hr*(360/12))+(min*(360/60)/12);
+let minPos = (min*(360/60))+(sec*(360/60)/60);
+let secPos = sec*(360/60);
+// console.log(hrPos,minPos,secPos)
 
 function runClock() {
-  var date = new Date();
-
-  let hr = date.getHours();
-  let min = date.getMinutes();
-  let sec = date.getSeconds();
-  console.log("The Time is " + hr + ":" + min + ":" + sec)
-
-  let hrPos = (hr*(360/12))+(min*(360/60)/12);
-  let minPos = (min*(360/60))+(sec*(360/60)/60);
-  let secPos = sec*(360/60);
-
-  console.log(hrPos,minPos,secPos)
+  hrPos = hrPos + (3/360);
+  minPos = minPos + (6/60);
+  secPos = secPos + 6;
 
   HOURHAND.style.transform = "rotate(" + hrPos + "deg)";
   MINUTEHAND.style.transform = "rotate(" + minPos + "deg)";
