6a721693e1f3e3c4a95164c51aebf857d7da6ccb
guachochi <davis.bradleyj@gmail.com>
2020-12-15 11:30:29 -0800

    Write logic to determine the time

--- vanillaJS-practice/ch6/script.js
+++ vanillaJS-practice/ch6/script.js
@@ -1,3 +1,18 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPos, minPos, secPos;
+
+var date = new Date();
+console.log(date);
+
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("The Time is " + hr + ":" + min + ":" + sec)
+
+HOURHAND.style.transform = "rotate(" + hrPos + " deg)";
+MINUTEHAND.style.transform = "rotate(" + minPos + " deg)";
+SECONDHAND.style.transform = "rotate(" + secPos + " deg)";
+
