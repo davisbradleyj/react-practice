a67f743f281b070d261fe11232bf1d70faf49060
guachochi <davis.bradleyj@gmail.com>
2020-12-15 11:54:53 -0800

    Set clock to run using a one second interval through a js function

--- vanillaJS-practice/ch6/script.js
+++ vanillaJS-practice/ch6/script.js
@@ -2,17 +2,24 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPos, minPos, secPos;
 
-var date = new Date();
-console.log(date);
+function runClock() {
+  var date = new Date();
 
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log("The Time is " + hr + ":" + min + ":" + sec)
+  let hr = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  console.log("The Time is " + hr + ":" + min + ":" + sec)
 
-HOURHAND.style.transform = "rotate(" + hrPos + " deg)";
-MINUTEHAND.style.transform = "rotate(" + minPos + " deg)";
-SECONDHAND.style.transform = "rotate(" + secPos + " deg)";
+  let hrPos = (hr*(360/12))+(min*(360/60)/12);
+  let minPos = (min*(360/60))+(sec*(360/60)/60);
+  let secPos = sec*(360/60);
 
+  console.log(hrPos,minPos,secPos)
+
+  HOURHAND.style.transform = "rotate(" + hrPos + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + minPos + "deg)";
+  SECONDHAND.style.transform = "rotate(" + secPos + "deg)";
+}
+
+let interval = setInterval(runClock, 1000);
