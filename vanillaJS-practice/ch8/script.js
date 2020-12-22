beb7ca9a6edb10dc5fbb4ccb5906f41203fc07a6
guachochi <davis.bradleyj@gmail.com>
2020-12-22 11:40:10 -0800

    Add in a helper function to add a leading zero to times less than 9, passing through the timer[i] value

--- vanillaJS-practice/ch8/script.js
+++ vanillaJS-practice/ch8/script.js
@@ -7,11 +7,17 @@ const theTimer = document.querySelector(".timer");
 let timer = [0,0,0,0]
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
+function leadingZero(time) {
+  if (time <=9) {
+    time = "0" + time;
+  }
+  return time;
+}
 
 
 // Run a standard minute/second/hundredths timer:
 function runTimer(){
-  let currentTime = timer[0]+":"+timer[1]+":"+timer[2];
+  let currentTime = leadingZero(timer[0])+":"+leadingZero(timer[1])+":"+leadingZero(timer[2]);
   theTimer.innerHTML = currentTime;
   timer[3]++;
 
@@ -19,7 +25,6 @@ function runTimer(){
   timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
   timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
-
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
