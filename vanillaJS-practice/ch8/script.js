f05fc94227739dbcdd59501778161bcc40ff4952
guachochi <davis.bradleyj@gmail.com>
2020-12-22 11:46:44 -0800

    Add color indicator to text area to let tester know status of their entry

--- vanillaJS-practice/ch8/script.js
+++ vanillaJS-practice/ch8/script.js
@@ -4,11 +4,11 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-let timer = [0,0,0,0]
+let timer = [0, 0, 0, 0]
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
-  if (time <=9) {
+  if (time <= 9) {
     time = "0" + time;
   }
   return time;
@@ -16,19 +16,30 @@ function leadingZero(time) {
 
 
 // Run a standard minute/second/hundredths timer:
-function runTimer(){
-  let currentTime = leadingZero(timer[0])+":"+leadingZero(timer[1])+":"+leadingZero(timer[2]);
+function runTimer() {
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
   theTimer.innerHTML = currentTime;
   timer[3]++;
 
-  timer[0] = Math.floor((timer[3]/100)/60);
-  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
+  timer[0] = Math.floor((timer[3] / 100) / 60);
+  timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60));
   timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
   let textEntered = testArea.value;
+  let originTextMatch = originText.substring(0, textEntered.length);
+
+  if (textEntered == originText) {
+    testWrapper.style.borderColor = "#429890";
+  } else {
+    if (textEntered == originTextMatch) {
+      testWrapper.style.borderColor = "#65CCF3";
+    } else {
+      testWrapper.style.borderColor = "#E95D0F";
+    }
+  }
   console.log(textEntered)
 }
 
@@ -42,11 +53,11 @@ function start() {
 }
 
 // Reset everything:
-function reset(){
+function reset() {
   console.log("Reset button clicked");
 }
 
 // Event listeners for keyboard input and the reset button:
-testArea.addEventListener("keypress",start,false);
-testArea.addEventListener("keyup",spellCheck,false);
-resetButton.addEventListener("click",reset,false);
+testArea.addEventListener("keypress", start, false);
+testArea.addEventListener("keyup", spellCheck, false);
+resetButton.addEventListener("click", reset, false);
