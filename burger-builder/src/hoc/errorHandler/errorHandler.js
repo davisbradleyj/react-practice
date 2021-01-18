58b2db25866d25447359408e2c41a13aa939c19d
guachochi <davis.bradleyj@gmail.com>
2021-01-18 10:24:54 -0800

    Build logic to handle errors as a higher order component

--- burger-builder/src/hoc/errorHandler/errorHandler.js
+++ burger-builder/src/hoc/errorHandler/errorHandler.js
@@ -18,6 +18,10 @@ const errorHandler = (WrappedComponent, axios) => {
       });
     }
 
+    componentWillUnmount () {
+      
+    }
+
     errorConfirmedHandler = () => {
       this.setState({ error: null });
     }
