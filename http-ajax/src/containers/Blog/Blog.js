155ada73c1ed6373b9ca07e0d515832173b1a3f5
guachochi <davis.bradleyj@gmail.com>
2021-01-19 11:18:12 -0800

    Modify Route to use component reference rather than render

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -18,8 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" exact render={() => <h1>Home2</h1>} />
+                <Route path="/" exact component={Posts} />
+                <Route path="/" render={() => <h1>Home2</h1>} />
                 {/* <section>
                     <FullPost id={this.state.selectedPostId} />
                 </section>
