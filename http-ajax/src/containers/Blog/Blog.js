4f236c1b5533805434755c6eda306b3d8c03f0d1
guachochi <davis.bradleyj@gmail.com>
2021-01-26 10:54:49 -0800

    Enable ESLint, revise formatting for any affected file

--- http-ajax/src/containers/Blog/Blog.js
+++ http-ajax/src/containers/Blog/Blog.js
@@ -9,42 +9,42 @@ import NewPost from '../NewPost/NewPost';
 import FullPost from '../FullPost/FullPost';
 
 class Blog extends Component {
-    render() {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><NavLink
-                                to="/"
-                                exact
-                                activeClassName="my-active"
-                                activeStyle={{
-                                    color: '#fa923f',
-                                    textDecoration: 'underline'
-                                }}>Home</NavLink></li>
-                            <li><NavLink to={{
-                                pathname: '/new-post',
-                                // hash: '#submit',
-                                // search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                <Switch> 
-                    <Route path="/" exact component={Posts} />
-                    <Route path="/new-post" component={NewPost} />
-                    <Route path="/:id" exact component={FullPost} />
-                </Switch>
-                {/* <section>
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink
+                to="/"
+                exact
+                activeClassName="my-active"
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}>Home</NavLink></li>
+              <li><NavLink to={{
+                pathname: '/new-post',
+                // hash: '#submit',
+                // search: '?quick-submit=true'
+              }}>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          <Route path="/" exact component={Posts} />
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/:id" exact component={FullPost} />
+        </Switch>
+        {/* <section>
                     <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
                 </section> */}
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default Blog;
